dcf3a088e52107bddc351432350ba55945b4fa48
jackyrwj <763214954@qq.com>
2023-05-17 23:20:44 +0800

    add 开发模拟API接口(yuapi-interface) add 开发调用接口客户端 add 客户端SDK的开发(yuapi-client-sdk)

---
+++ yuapi-client-sdk/src/main/java/com/yupi/yuapiclientsdk/YuApiClientConfig.java
@@ -0,0 +1,23 @@
+package com.yupi.yuapiclientsdk;
+
+import com.yupi.yuapiclientsdk.client.YuApiClient;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties("yuapi.client")
+@ComponentScan
+public class YuApiClientConfig {
+    private String accessKey;
+
+    private String secretKey;
+
+    @Bean
+    public YuApiClient yuApiClient(){
+        return new YuApiClient(accessKey,secretKey);
+    }
+}
